010477c77203435fe8d68331e1f09c5fdbf211ac
Sooji Son <soojison@hotmail.com>
2016-02-11 22:04:18 -0600

    finish hw1

--- week1/hw1.hs
+++ week1/hw1.hs
@@ -30,3 +30,12 @@ sumDigits (x:xs) = (x `div` 10) + (x `mod` 10) + sumDigits(xs)
 -- Exercise 4
 validate :: Integer -> Bool
 validate n = sumDigits (doubleEveryOther (toDigits n)) `mod` 10 == 0
+
+-- Exercise 5
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ (hanoi 1 a b c) ++ (hanoi (n-1) c b a)
+
