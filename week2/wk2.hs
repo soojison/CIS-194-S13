b095d2c3d9628d1bc789907d9cd042b11ae5d62a
Sooji Son <soojison96@gmail.com>
2016-02-11 22:21:10 -0600

    finish enum types

--- week2/wk2.hs
+++ week2/wk2.hs
@@ -9,3 +9,25 @@ data Thing = Shoe
     deriving Show
 -- This declares a new type called Thing with 5 data constructors
 
+shoe :: Thing
+shoe = Shoe
+
+listO'Things :: [Thing]
+listO'Things = [Shoe, SealingWax, King, Cabbage, King]
+
+-- functions on Things by pattern-matching
+isSmall :: Thing -> Bool
+isSmall Shoe       = True
+isSmall Ship       = False
+isSmall SealingWax = True
+isSmall Cabbage    = True
+isSmall King       = False
+
+-- we could also make the def. of isSmall shorter
+-- because function clauses are tried in order from top to bottom
+isSmall2 :: Thing -> Bool
+isSmall2 Ship = False
+isSmall2 King = False
+isSmall2 _    = True
+
+-- Beyond enumerations
