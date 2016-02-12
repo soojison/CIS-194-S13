82a116232b2749cd6a19dae100a5b70220d5ca3f
Sooji Son <soojison@hotmail.com>
2016-02-11 22:10:38 -0600

    start week 2 lecture

---
+++ week2/wk2.hs
@@ -0,0 +1,11 @@
+-- Week 2 of CIS 194
+
+-- Enumeration types
+data Thing = Shoe
+            | Ship
+            | SealingWax
+            | Cabbage
+            | King
+    deriving Show
+-- This declares a new type called Thing with 5 data constructors
+
